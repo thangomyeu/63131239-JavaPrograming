2a7e5c1b8ebce6fa5a11f110ab307e75c065ea05
thangomyeu <thichvandong@gmail.com>
2025-02-19 19:29:38 +0700

    update

---
+++ bai1_helloWorld/src/bai1_helloWorld/helpworld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package bai1_helloWorld;
+
+/**
+ * 
+ */
+public class helpworld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("helloworld");
+		// TODO Auto-generated method stub
+
+	}
+
+}
