5b69a9b86345c9bd7d247de1e6d34ca1c39bf4ad
thangomyeu <thichvandong@gmail.com>
2025-02-24 13:38:02 +0700

    update baitapphan2

---
+++ vd1phan2/app/src/main/java/ntu/lechinhthang/vd1phan2/MainActivity.java
@@ -0,0 +1,14 @@
+package ntu.lechinhthang.vd1phan2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); //nap layout
+    }
+}
